7391146cdf8a3fd756a4d26d995f49456bb754a1
akshayitzme <akshayitzme.official@gmail.com>
2023-10-31 12:39:22 +0530

    refactor: videoTextCard component

--- src/app/solutions/retail/page.jsx
+++ src/app/solutions/retail/page.jsx
@@ -1,128 +1,38 @@
-"use client"
-import { useState } from 'react';
-import Hero from '@/components/Hero'
-import React from 'react'
+'use client';
+import Hero from '@/components/Hero';
+import React, { useState } from 'react';
 import VideoTextCard from '@/components/VideoTextCard';
 import cardData from '../../../textContent/retail/cardOne.json';
 import card2Data from '../../../textContent/retail/cardTwo.json';
 import card3Data from '../../../textContent/retail/cardThree.json';
 
-const page = () => {
-
-  const [selectedTab, setSelectedTab] = useState('POSAwesome');
-  const [selectedTab2, setSelectedTab2] = useState('Warehouse stock management');
-
-  const [selectedTab3, setSelectedTab3] = useState(' NeuPOS');
-
+const Page = () => {
   return (
     <>
       <Hero />
-      <div
 
-        id="main"
-        className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
-      >
-
-        <div
-          id="hero"
-          className="bg-blue-500 bg-opacity-20 max-h-[100rem] md:flex md:items-center md:justify-center mb-16 mt-10 py-10 px-5 rounded-3xl md:max-w-[100vw] md:px-8 "
-        >
-          <div className="md:mt-[0.6vw]">
-            <div
-              id="tab"
-              className="flex items-center justify-around md:justify-start md:gap-10 text-sm font-bold gap-5 overflow-x-auto"
-            >
-              <h1
-                onClick={() => setSelectedTab('Warehouse stock management')}
-                className={`${selectedTab === 'Warehouse stock management' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800`}
-              >
-                Warehouse stock management
-              </h1>
-              <h1
-                onClick={() => setSelectedTab('Inventory Forecasting')}
-                className={`${selectedTab === 'Inventory Forecasting' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800 `}
-              >
-Inventory Forecasting
-              </h1>
-              <h1
-                onClick={() => setSelectedTab('Price Lists')}
-                className={`${selectedTab === 'Price Lists' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800`}
-              >
-                Price Lists
-              </h1>
-
-            </div>
-            <VideoTextCard data={cardData[selectedTab]} />
-          </div>
-        </div>
-      </div>
       <div
         id="main"
         className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
       >
-
-        <div
-          id="hero"
-          className="bg-blue-500 bg-opacity-20 max-h-[100rem] md:flex md:items-center md:justify-center mb-16 mt-10 py-10 px-5 rounded-3xl md:max-w-[100vw] md:px-8 "
-        >
-          <div className="md:mt-[0.6vw]">
-            <div
-              id="tab"
-              className="flex items-center justify-around md:justify-start md:gap-10 text-sm font-bold gap-5 overflow-x-auto"
-            >
-              <h1
-                onClick={() => setSelectedTab2('POSAwesome')}
-                className={`${selectedTab2 === 'POSAwesome' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800`}
-              >
-                POSAwesome
-              </h1>
-              <h1
-                onClick={() => setSelectedTab2('Custom Invoice Print Format Templates')}
-                className={`${selectedTab2 === 'Custom Invoice Print Format Templates' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800 `}
-              >
-                Custom Invoice Print Format Templates
-              </h1>
-              <h1
-                onClick={() => setSelectedTab2('Coupon Codes and Loyalty')}
-                className={`${selectedTab2 === 'Coupon Codes and Loyalty' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800`}
-              >
-                Coupon Codes and Loyalty
-              </h1>
-
-            </div>
-            <VideoTextCard data={card2Data[selectedTab2]} />
-          </div>
-        </div>
+        <VideoTextCard data={cardData} />
       </div>
+
       <div
         id="main"
         className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
       >
-
-        <div
-          id="hero"
-          className="bg-blue-500 bg-opacity-20 max-h-[100rem] md:flex md:items-center md:justify-center mb-16 mt-10 py-10 px-5 rounded-3xl md:max-w-[100vw] md:px-8 "
-        >
-          <div className="md:mt-[0.6vw]">
-            <div
-              id="tab"
-              className="flex items-center justify-around md:justify-start md:gap-10 text-sm font-bold gap-5 overflow-x-auto"
-            >
-              <h1
-                onClick={() => setSelectedTab3(' NeuPOS ')}
-                className={`${selectedTab3 === 'NeuPOS' ? 'border-b-4 border-blue-gray-800' : ''} hover:border-b-4 transition duration-500 hover:border-blue-gray-800`}
-              >
-                NeuPOS
-              </h1>
-
-            </div>
-            <VideoTextCard data={card3Data[selectedTab3]} />
-          </div>
-        </div>
+        <VideoTextCard data={card2Data} />
       </div>
 
+      <div
+        id="main"
+        className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
+      >
+        <VideoTextCard data={card3Data} />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
--- src/components/VideoTextCard.jsx
+++ src/components/VideoTextCard.jsx
@@ -1,68 +1,92 @@
-import React from 'react';
-function VideoTextCard(props) {
-  const bodyText = props.data.body.split('\n');
-  
+import React, { useState } from 'react';
+function VideoTextCard({ data }) {
+  const tabs = Object.keys(data);
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
   return (
-    <React.Fragment>
-      <div className="md:mt-[0.6vw] md:flex  w-full">
-        <div className=' '>
-          <div id="video" className='rounded-2xl md:p-4 md:mt-3 mt-6 p-10'>
+    <div
+      id="hero"
+      className="bg-blue-500 bg-opacity-20 max-h-[100rem] md:flex md:items-center md:justify-center mb-16 mt-10 py-10 px-5 rounded-3xl md:max-w-[100vw] md:px-8 "
+    >
+      <div className="md:mt-[0.6vw]">
+        {tabs.length > 1 ? (
+          <div
+            id="tab"
+            className="flex items-center justify-around md:justify-start md:gap-10 text-sm font-bold gap-5 overflow-x-auto"
+          >
+            {tabs.map((tab) => (
+              <h1
+                key={tab}
+                onClick={() => setSelectedTab(tab)}
+                className={`${
+                  selectedTab === tab ? 'border-b-4 border-blue-gray-800' : ''
+                } hover:border-b-4 transition duration-500 hover:border-blue-gray-800`}
+              >
+                {tab}
+              </h1>
+            ))}
+          </div>
+        ) : null}
+
+        <div className="md:mt-[0.6vw] md:flex  w-full">
+          <div className=" ">
+            <div id="video" className="rounded-2xl md:p-4 md:mt-3 mt-6 p-10">
+              <video
+                src={data[selectedTab].video}
+                alt="img"
+                autoPlay
+                loop
+                muted
+                width={1000}
+                height={100}
+                className="md:w-full md:h-full object-cover cursor-pointer w- rounded-xl bg-black md:hidden block "
+                id="topimg"
+              />
+            </div>
+            <div id="title" className=" text-center md:text-left mt-10 ">
+              <h1 className="text-4xl font-bold text-[#212121]">
+                {data[selectedTab].heading}
+              </h1>
+            </div>
+            <div
+              id="text"
+              className="p-2 mt-2 rounded-3xl text-lg font-semibold leading-6"
+            >
+              {data[selectedTab].body.split('\n').map((i, index) => (
+                <p key={index} className="mt-4">
+                  {i}
+                </p>
+              ))}
+
+              {data[selectedTab].points
+                ? data[selectedTab].points.map((i, index) => <p key={index}>{i}</p>)
+                : null}
+            </div>
+            <div
+              id="btns"
+              className="flex items-center justify-center md:block p-3"
+            >
+              <button className="bg-blue-300 py-2 px-4 md:py-3 md:px-7 rounded-lg font-bold text-white">
+                Dummybtn
+              </button>
+            </div>
+          </div>
+          <div className="md:w-100vw bgblack md:pb-10 md:px-16">
             <video
-              src={props.data.video}
+              src={data[selectedTab].video}
               alt="img"
               autoPlay
               loop
               muted
-              width={1000}
+              width={100}
               height={100}
-              className="md:w-full md:h-full object-cover cursor-pointer w- rounded-xl bg-black md:hidden block "
-              id="topimg"
+              className="md:w-[100%] md:h-full rounded-2xl bg mt-9 ml-8 cursor-pointer   md:block hidden"
+              id="bottomimg"
             />
           </div>
-          <div id="title" className=" text-center md:text-left mt-10 ">
-            <h1 className="text-4xl font-bold text-[#212121]">
-              {props.data.heading}
-            </h1>
-          </div>
-          <div
-            id="text"
-            className="p-2 mt-2 rounded-3xl text-lg font-semibold leading-6"
-          >
-            {bodyText.map((i, index) => (
-              <p key={index} className="mt-4">{i}</p>
-            ))}
-
-
-            {props.data.points ? props.data.points.map((i, index) => <p key={index}>
-
-              {i}</p>) : null
-            }
-
-          </div>
-          <div
-            id="btns"
-            className="flex items-center justify-center md:block p-3"
-          >
-            <button className="bg-blue-300 py-2 px-4 md:py-3 md:px-7 rounded-lg font-bold text-white">
-              Dummybtn
-            </button>
-          </div>
-        </div>
-        <div className='md:w-100vw bgblack md:pb-10 md:px-16'>
-        <video
-          src={props.data.video}
-          alt="img"
-          autoPlay
-          loop
-          muted
-          width={100}
-          height={100}
-          className="md:w-[100%] md:h-full rounded-2xl bg mt-9 ml-8 cursor-pointer   md:block hidden"
-          id="bottomimg"
-        />
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 }
 
