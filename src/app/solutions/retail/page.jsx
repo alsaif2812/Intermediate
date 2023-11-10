fb3e889dc265a9116bfcded6471c9ffeb2585365
alsaif2812 <alsaifsiddiqui1@gmail.com>
2023-11-10 14:44:14 +0530

    ded

--- src/app/solutions/retail/page.jsx
+++ src/app/solutions/retail/page.jsx
@@ -15,21 +15,21 @@ const Page = () => {
         id="main"
         className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
       >
-        <VideoTextCard data={cardData} />
+        <VideoTextCard data={cardData} bgColor="bg-[#898AA6] bg-opacity-30" />
       </div>
 
       <div
         id="main"
         className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
       >
-        <VideoTextCard data={card2Data} />
+        <VideoTextCard data={card2Data} bgColor="bg-[#FFF8EA] bg-opacity-30" />
       </div>
 
       <div
         id="main"
         className="p-5 md:flex md:items-center md:justify-center  md:px-[18vw]"
       >
-        <VideoTextCard data={card3Data} />
+        <VideoTextCard data={card3Data} bgColor="bg-[#FF8AAE] bg-opacity-30" />
       </div>
     </>
   );
