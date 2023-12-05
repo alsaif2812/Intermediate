cce8fff61e6f6fc1ba9b932e7fe0f4437f794f14
akshayitzme <akshayitzme.official@gmail.com>
2023-12-05 10:33:18 +0530

    fix: fontawesome icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,9 @@ import Head from 'next/head';
 import 'remixicon/fonts/remixicon.css';
 import logo from '../../public/images/wit-logo.png'
 import './globals.css'; // Import your global styles
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const nunito = Nunito({
   subsets: ['latin'],
