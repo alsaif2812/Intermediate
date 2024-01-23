6d084490c4a2f031fffb3b6b4fdb4444997b69b5
alsaif2812 <alsaifsiddiqui1@gmail.com>
2024-01-23 10:20:49 +0530

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ import logo from '../../public/images/favicon.png';
 const Footer = () => {
     return (
         <Fragment>
+            alsaif
             <footer className=" text-gray-600 body-font bg-[#fafafa]">
                 <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col ">
                     <div className=" flex-shrink-0 md:mx-0 mx-auto  md:text-left justify-center md:flex flex-col md:items-left md:items-start items-center  flex ">
